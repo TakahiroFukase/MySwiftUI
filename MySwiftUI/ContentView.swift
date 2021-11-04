69554aaf8f087b0384c13ebdc06c71bcd56eb269
Takahiro Fukase <fukase0622@yahoo.co.jp>
2021-11-04 23:03:03 +0900

    First commit.

---
+++ MySwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MySwiftUI
+//
+//  Created by Takahiro Fukase on 2021/11/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .foregroundColor(Color.white)
+            .padding(50)
+            .background(Color.green)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
