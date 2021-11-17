cee7df39c7716404e548e3e760b9840c63c20e8a
Takahiro Fukase <fukase0622@yahoo.co.jp>
2021-11-17 13:31:24 +0900

    LoremViewを作成、最初に表示される画面として設定

---
+++ MySwiftUI/UI/LoremView.swift
@@ -0,0 +1,20 @@
+//
+//  LoremView.swift
+//  MySwiftUI
+//
+//  Created by Takahiro Fukase on 2021/11/17.
+//
+
+import SwiftUI
+
+struct LoremView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct LoremView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoremView()
+    }
+}
