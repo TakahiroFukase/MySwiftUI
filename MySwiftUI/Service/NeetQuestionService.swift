29412ea2d6b7079acf57fedc78a313684da7e27c
Takahiro Fukase <fukase0622@yahoo.co.jp>
2021-11-18 21:32:00 +0900

    ニート診断の質問一覧を取得するサービスクラスを作成

---
+++ MySwiftUI/Service/NeetQuestionService.swift
@@ -0,0 +1,44 @@
+//
+//  NeetQuestionService.swift
+//  MySwiftUI
+//
+//  Created by Takahiro Fukase on 2021/11/17.
+//
+
+import Foundation
+
+/// ニート診断の質問一覧を取得するサービスクラス
+class NeetQuestionService {
+    
+    let url = URL(string: "https://takahirofukase.com/sample/questions.json")
+    
+    func getAllQuestions(res: @escaping ([NeetQuestion])->()) {
+        
+        guard let url = url else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            guard let data = data else { return }
+            
+            guard let neetQuestions = try? JSONDecoder().decode([NeetQuestion].self, from: data) else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                // 取得したテキストを呼び出し元に渡す
+                res(neetQuestions)
+            }
+            
+        }.resume()
+    }
+}
+
+struct NeetQuestion: Codable, Identifiable {
+    
+    var id: UUID? = UUID()
+    
+    // APIから取得する情報は以下の3項目
+    let text: String
+    let yesIsNeet: Bool
+    let extraNeetPoint: Int
+}
