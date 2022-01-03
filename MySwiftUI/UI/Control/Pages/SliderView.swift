8ea201f5d94fc7f46135df43b0ac11e00625285e
Takahiro Fukase <fukase0622@yahoo.co.jp>
2022-01-03 23:30:36 +0900

    SliderViewでスライダーを増やし、明るさを調節できるエリアが区分けになるように実装

--- MySwiftUI/UI/Control/Pages/SliderView.swift
+++ MySwiftUI/UI/Control/Pages/SliderView.swift
@@ -11,44 +11,72 @@ struct SliderView: View {
     
     @Environment(\.presentationMode) var presentationMode
     
-    @State var sliderValue = 0.0
+    @State var topSliderValue = 0.0
+    @State var middleSliderValue = 0.0
+    @State var bottomSliderValue = 0.0
+    
+    let imageWidth = UIScreen.main.bounds.width - 50
+    let imageHeight = (UIScreen.main.bounds.width - 50) / 1096 * 1644
     
     var body: some View {
         
-        Spacer(minLength: 50).fixedSize()
-        
-        ZStack {
+        VStack {
+
+            ZStack {
+                
+                Image(uiImage: UIImage(named: "sample")!)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: imageWidth,
+                           height: imageHeight)
+
+                VStack {
+                    
+                    Text("")
+                        .frame(width: imageWidth,
+                               height: imageHeight / 3)
+                        .background(Color(red: 0, green: 0, blue: 0, opacity: 1 - (topSliderValue / 100)))
+                    
+                    Text("")
+                        .frame(width: imageWidth,
+                               height: imageHeight / 3)
+                        .background(Color(red: 0, green: 0, blue: 0, opacity: 1 - (middleSliderValue / 100)))
+                    
+                    Text("")
+                        .frame(width: imageWidth,
+                               height: imageHeight / 3)
+                        .background(Color(red: 0, green: 0, blue: 0, opacity: 1 - (bottomSliderValue / 100)))
+                }
+            }
             
-            Image(uiImage: UIImage(named: "sample")!)
-                .resizable()
-                .scaledToFit()
-                .frame(width: UIScreen.main.bounds.width,
-                       height: UIScreen.main.bounds.width / 1096 * 1644)
+            VStack {
+                
+                Slider(value: $topSliderValue, in: 0...100, step: 10)
+                    .padding(.horizontal, 20.0)
+                
+                Slider(value: $middleSliderValue, in: 0...100, step: 10)
+                    .padding(.horizontal, 20.0)
+                
+                Slider(value: $bottomSliderValue, in: 0...100, step: 10)
+                    .padding(.horizontal, 20.0)
+            }
             
-            Text("")
-                .frame(width: UIScreen.main.bounds.width,
-                       height: UIScreen.main.bounds.width / 1096 * 1644)
-                .background(Color(red: 0, green: 0, blue: 0, opacity: 1 - (sliderValue / 100)))
+            Spacer()
+            
+            Button(action: {
+                // この画面を閉じる
+                presentationMode.wrappedValue.dismiss()
+            }, label: {
+                Text("閉じる")
+                    .bold()
+                    .frame(width: 200, height: 50)
+                    .foregroundColor(.white)
+                    .background(Color.gray)
+                    .cornerRadius(25)
+            })
+            
+            Spacer(minLength: 50).fixedSize()
         }
-        
-        Slider(value: $sliderValue, in: 0...100, step: 10)
-            .padding(20)
-        
-        Spacer()
-        
-        Button(action: {
-            // この画面を閉じる
-            presentationMode.wrappedValue.dismiss()
-        }, label: {
-            Text("閉じる")
-                .bold()
-                .frame(width: 200, height: 50)
-                .foregroundColor(.white)
-                .background(Color.gray)
-                .cornerRadius(25)
-        })
-        
-        Spacer(minLength: 50).fixedSize()
     }
 }
 
