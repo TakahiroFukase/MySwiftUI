be8c3ae19069d843463d0114127efbd6d5701b5f
Takahiro Fukase <fukase0622@yahoo.co.jp>
2022-01-08 10:22:29 +0900

    StepperViewの画面コントロールをVStackに含める

--- MySwiftUI/UI/Control/Pages/StepperView.swift
+++ MySwiftUI/UI/Control/Pages/StepperView.swift
@@ -15,47 +15,50 @@ struct StepperView: View {
     
     var body: some View {
         
-        Spacer(minLength: 50).fixedSize()
-        
-        Stepper("商品数を選択（最大5つまで）", onIncrement: {
-            if items.count < 5 {
-                items.append("商品")
+        VStack {
+            
+            Spacer(minLength: 50).fixedSize()
+            
+            Stepper("商品数を選択（最大5つまで）", onIncrement: {
+                if items.count < 5 {
+                    items.append("商品")
+                }
+            }, onDecrement: {
+                if 0 < items.count {
+                    items.removeLast()
+                }
+            })
+            .padding(.horizontal, 20)
+            
+            Divider()
+            
+            Spacer(minLength: 20).fixedSize()
+            
+            ForEach(items, id: \.self) { item in
+                VStack {
+                    Text(item)
+                        .padding(10)
+                    
+                    Divider()
+                }
             }
-        }, onDecrement: {
-            if 0 < items.count {
-                items.removeLast()
-            }
-        })
-        .padding(.horizontal, 20)
-        
-        Divider()
-        
-        Spacer(minLength: 20).fixedSize()
-        
-        ForEach(items, id: \.self) { item in
-            VStack {
-                Text(item)
-                    .padding(10)
-                
-                Divider()
-            }
+            
+            Spacer()
+            
+            Button(action: {
+                // この画面を閉じる
+                presentationMode.wrappedValue.dismiss()
+            }, label: {
+                Text("閉じる")
+                    .bold()
+                    .frame(width: 200, height: 50)
+                    .foregroundColor(.white)
+                    .background(Color.gray)
+                    .cornerRadius(25)
+            })
+            
+            Spacer(minLength: 50).fixedSize()
         }
-        
-        Spacer()
-        
-        Button(action: {
-            // この画面を閉じる
-            presentationMode.wrappedValue.dismiss()
-        }, label: {
-            Text("閉じる")
-                .bold()
-                .frame(width: 200, height: 50)
-                .foregroundColor(.white)
-                .background(Color.gray)
-                .cornerRadius(25)
-        })
-        
-        Spacer(minLength: 50).fixedSize()
     }
 }
 
